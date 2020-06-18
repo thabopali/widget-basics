a74f5ba46d883ac36a695c964dd2e69b51e95246
Pali Thabo <tpali@cut.ac.za>
2020-06-18 16:03:28 +0200

    Changed displayed text dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var _messageIndex = 0;
+  final _messages = const [
+    'This is a different message',
+    'Showing this message because I was clicked',
+    'One of the messages to display',
+    'This is the last message to be added here',
+  ];
+
+  void _changeMessages(){
+    setState(() {
+      _messageIndex += 1;
+      print('Index is increment: ');
+      print( _messageIndex);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,12 +33,15 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text('Widget Basics'),
       ),
-      body: Column(
+      body: _messageIndex < _messages.length ? Column(
         children: <Widget>[
-          Text('Hello I\'m testing widgets'),
-          RaisedButton(child: Text('Change Text'), onPressed: null,)
+          Text(_messages[_messageIndex]),
+          RaisedButton(
+            child: Text('Change Text'),
+            onPressed: _changeMessages,
+          )
         ],
-      ),
+      ): Text('Stop clicking the button now!'),
     ));
   }
 }
